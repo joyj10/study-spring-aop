02147ca845529306d5b56c0e025463657d67d648
dev_one <micool1030@gmail.com>
2024-01-02 19:38:44 +0900

    [feat] 포인트컷 분리

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,7 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 class AopTest {
     @Autowired
     OrderService orderService;
