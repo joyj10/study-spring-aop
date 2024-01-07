de518220b251fd8f8b36ab07ecb5d40e8ec67747
dev_one <micool1030@gmail.com>
2024-01-07 10:36:09 +0900

    [feat] retry annotation 개발 및 적용

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -11,8 +12,10 @@ public class ExamRepository {
     /**
      * 5번에 1번 실패 하는 요청
      * : retry 테스트 위한 설정
+     * retry 조심해서 사용해야 함 max가 꼭 필요하며, 간헐적으로 발생하고, 재시도해도 이슈가 없을 때 사용해야 함
      */
     @Trace
+    @Retry
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
---
+++ src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -0,0 +1,31 @@
+package hello.aop.exam.aop;
+
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+    @Around("@annotation(retry)") //타입 정보가 들어감
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {} retry={}", joinPoint.getSignature(), retry);
+
+        int maxRetry = retry.value();
+        Exception exceptionHolder = null;
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count={}/{}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            } catch (Exception e) { //Throwable 은 Exception 보다 상위(개발자가 처리하는 건 Ex 정도) Throwable 자바 메모리 문제 등으로 처리 어려움
+                exceptionHolder = e;
+            }
+        }
+
+        throw exceptionHolder;
+    }
+}
